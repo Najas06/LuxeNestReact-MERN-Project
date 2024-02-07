31bc443dc48165eed146285401282d3926d245a2
Najas06 <najasneju@gmail.com>
2024-02-07 10:41:49 +0530

    #

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 // create and run server by express ,  nodemon , node.js 
 import express from 'express';
 
+// connecting mongoose 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MONGODB");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
 const app = express();
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Server is running on port 3000!!!!');
 })
